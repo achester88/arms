2a0e4276f5fbb9faf8471f0b1c502d922cc7606c
achester88 <anthonychester71@gmail.com>
2024-07-17 16:21:42 -0400

    upload init

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use actix_web::{HttpServer, App, web, HttpResponse, Responder};
+use tera::{Tera, Context};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Post {
+    title: String,
+    link: String,
+    author: String,
+}
+
+async fn index(tera: web::Data<Tera>) -> impl Responder {
+    let mut data = Context::new();
+
+    let posts = [
+        Post {
+            title: String::from("This is the first link"),
+            link: String::from("https://example.com"),
+            author: String::from("Bob")
+        },
+        Post {
+            title: String::from("The Second Link"),
+            link: String::from("https://example.com"),
+            author: String::from("Alice")
+        },
+    ];
+    
+    data.insert("title", "ARMS Portal");
+    data.insert("posts", &posts);
+
+    let rendered = tera.render("index.html", &data).unwrap();
+    HttpResponse::Ok().body(rendered)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let tera = Tera::new("templates/**/*").unwrap();
+        App::new()
+            .data(tera)
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
